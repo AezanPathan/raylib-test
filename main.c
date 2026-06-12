eb98f488c8156c8c251b0f30292feead6dd729fe
aezanpathan <aezanp@gmail.com>
2026-06-13 00:05:01 +0530

    i am somewhere

--- main.c
+++ main.c
@@ -96,12 +96,15 @@ int main(void)
     Material backMat = LoadMaterialDefault();
     backMat.maps[MATERIAL_MAP_DIFFUSE].texture = back;
 
-    // Mesh quadMesh = GenMeshPlane(2.0f, 4.0f, 1, 1);
     Mesh quadMesh = GenMeshPlane(1.0f, 2.0f, 1, 1);
 
     Material material = LoadMaterialDefault();
     material.maps[MATERIAL_MAP_DIFFUSE].texture = hero;
 
+    frontMat.maps[MATERIAL_MAP_DIFFUSE].texture = front;
+    frontMat.maps[MATERIAL_MAP_DIFFUSE].color = WHITE;
+
+    frontMat.shader = LoadShader(0, 0);
     int direction = 0;
 
     SetTargetFPS(60);
@@ -110,34 +113,64 @@ int main(void)
     Rectangle sideFrame = {64, 0, 32, 32};
     Rectangle backFrame = {128, 0, 32, 32};
 
-   Matrix frontTransform =
-    MatrixMultiply(
-        MatrixRotateX(DEG2RAD * 90.0f),
-        MatrixTranslate(0.0f, 2.0f, 0.10f));
+    // Discard plan background for the sprites with the custom shader
+    Shader alphaDiscard = LoadShader("shaders/alpha_discard.vs", "shaders/alpha_discard.fs");
 
-Matrix sideTransform =
-    MatrixMultiply(
-        MatrixRotateY(DEG2RAD * 90.0f),
-        MatrixMultiply(
-            MatrixRotateX(DEG2RAD * 90.0f),
-            MatrixTranslate(0.10f, 2.0f, 0.0f)));
+    frontMat.shader = alphaDiscard;
+    sideMat.shader = alphaDiscard;
+    backMat.shader = alphaDiscard;
 
-Matrix backTransform =
-    MatrixMultiply(
-        MatrixRotateY(DEG2RAD * 180.0f),
-        MatrixMultiply(
-            MatrixRotateX(DEG2RAD * 90.0f),
-            MatrixTranslate(0.0f, 2.0f, -0.10f)));
+    Material leftMat = LoadMaterialDefault();
+    leftMat.maps[MATERIAL_MAP_DIFFUSE].texture = side;
+
+    Material rightMat = LoadMaterialDefault();
+    rightMat.maps[MATERIAL_MAP_DIFFUSE].texture = side;
+
+    leftMat.shader = alphaDiscard;
+    rightMat.shader = alphaDiscard;
+
+    float shellDepth = 0.15f; // define a shell thickness
+
+    // for the debug purpose
+    float frontOffsetX = 0.0f;
+    float frontOffsetY = 1.0f;
+    float frontOffsetZ = 0.12f;
+
+    float sideOffsetX = 0.0f;
+    float sideOffsetY = 1.0f;
+    float sideOffsetZ = 0.00f;
+
+    float backOffsetX = 0.0f;
+    float backOffsetY = 1.0f;
+    float backOffsetZ = -0.12f;
+
+    // rotation
+    float frontRotX = 90.0f;
+    float frontRotY = 0.0f;
+    float frontRotZ = 0.0f;
+
+    float sideRotX = 90.0f;
+    float sideRotY = 0.0f;
+    float sideRotZ = 0.0f;
+
+    float backRotX = 90.0f;
+    float backRotY = 0.0f;
+    float backRotZ = 0.0f;
 
     while (!WindowShouldClose())
     {
         // Update
         UpdatePlayer(&player);
 
+        if (IsKeyDown(KEY_Q))
+            cameraAngle -= 0.02f;
+
+        if (IsKeyDown(KEY_E))
+            cameraAngle += 0.02f;
+
         direction = GetDirectionFromAngle(cameraAngle, 8);
 
-        float mouseDelta =
-            GetMouseDelta().x;
+        float mouseDelta = GetMouseDelta().x;
 
         cameraAngle += mouseDelta * 0.003f;
 
@@ -149,6 +182,73 @@ Matrix backTransform =
 
         camera.target = player.position;
 
+        float billboardYaw = atan2f(
+            camera.position.x - player.position.x,
+            camera.position.z - player.position.z);
+
+        // Matrix frontTransform =
+        //     MatrixTranslate(
+        //         player.position.x + frontOffsetX,
+        //         player.position.y + frontOffsetY,
+        //         player.position.z + frontOffsetZ);
+
+        // Matrix sideTransform =
+        //     MatrixTranslate(
+        //         player.position.x + sideOffsetX,
+        //         player.position.y + sideOffsetY,
+        //         player.position.z + sideOffsetZ);
+
+        // Matrix backTransform =
+        //     MatrixTranslate(
+        //         player.position.x + backOffsetX,
+        //         player.position.y + backOffsetY,
+        //         player.position.z + backOffsetZ);
+
+        Matrix frontTransform =
+            MatrixMultiply(
+                MatrixRotateX(DEG2RAD * 90.0f),
+                MatrixTranslate(
+                    player.position.x,
+                    player.position.y + 1.0f,
+                    player.position.z + shellDepth));
+
+        Matrix backTransform =
+            MatrixMultiply(
+                MatrixRotateY(DEG2RAD * 180.0f),
+                MatrixMultiply(
+                    MatrixRotateX(DEG2RAD * 90.0f),
+                    MatrixTranslate(
+                        player.position.x,
+                        player.position.y + 1.0f,
+                        player.position.z - shellDepth)));
+
+        Matrix leftTransform =
+            MatrixMultiply(
+                MatrixRotateY(DEG2RAD * 90.0f),
+                MatrixMultiply(
+                    MatrixRotateX(DEG2RAD * 90.0f),
+                    MatrixTranslate(
+                        player.position.x - shellDepth,
+                        player.position.y + 1.0f,
+                        player.position.z)));
+
+        Matrix rightTransform =
+            MatrixMultiply(
+                MatrixRotateY(DEG2RAD * -90.0f),
+                MatrixMultiply(
+                    MatrixRotateX(DEG2RAD * 90.0f),
+                    MatrixTranslate(
+                        player.position.x + shellDepth,
+                        player.position.y + 1.0f,
+                        player.position.z)));
+        Matrix frontRotation = MatrixRotateX(DEG2RAD * 90.0f);
+        Matrix sideRotation = MatrixMultiply(MatrixRotateY(billboardYaw), MatrixRotateX(DEG2RAD * 90.0f));
+        Matrix backRotation = MatrixRotateX(DEG2RAD * 90.0f);
+
+        frontTransform = MatrixMultiply(frontRotation, frontTransform);
+      //  sideTransform = MatrixMultiply(sideRotation, sideTransform);
+        backTransform = MatrixMultiply(backRotation, backTransform);
+
         // Draw
         BeginDrawing();
         ClearBackground(WHITE);
@@ -156,6 +256,13 @@ Matrix backTransform =
         BeginMode3D(camera);
 
         DrawPlane((Vector3){0, 0, 0}, (Vector2){20, 20}, DARKGRAY);
+
+        //     DrawCube(
+        // (Vector3){0,1,0},
+        // 1.0f,
+        // 2.0f,
+        // 1.0f,
+        // RED);
 
         Rectangle source = {
             direction * 32,
@@ -168,12 +275,6 @@ Matrix backTransform =
             {0.3f, 0, 0},
             {-0.3f, 0, 0}};
 
-        // DrawBillboardRec(camera, hero, frontFrame, positions[0], (Vector2){2, 4}, WHITE);
-
-        //  DrawBillboardRec(camera, hero, sideFrame, positions[1], (Vector2){2, 4}, WHITE);
-
-        //  DrawBillboardRec(camera, hero, backFrame, positions[2], (Vector2){2, 4}, WHITE);
-
         float angleDegrees = cameraAngle * RAD2DEG;
 
         while (angleDegrees < 0)
@@ -182,73 +283,38 @@ Matrix backTransform =
         while (angleDegrees >= 360.0f)
             angleDegrees -= 360.0f;
 
-            rlDisableBackfaceCulling();
+        rlDisableBackfaceCulling();
 
-      //  if (angleDegrees < 120.0f)
-            DrawMesh(quadMesh, frontMat, frontTransform);
+        // //  if (angleDegrees < 120.0f)
+        // DrawMesh(quadMesh, frontMat, frontTransform);
 
-       // if (angleDegrees > 60.0f && angleDegrees < 300.0f)
-           DrawMesh(quadMesh, sideMat, sideTransform);
+        // //   if (angleDegrees > 60.0f && angleDegrees < 300.0f)
+        // DrawMesh(quadMesh, sideMat, sideTransform);
 
-      //  if (angleDegrees > 180.0f)
-          DrawMesh(quadMesh, backMat, backTransform);
+        // //  if (angleDegrees > 180.0f)
+        // DrawMesh(quadMesh, backMat, backTransform);
 
-          rlEnableBackfaceCulling();
-        //   DrawCube(
-        // (Vector3){0,2,0},
-        // 1.0f,
-        // 2.0f,
-        // 0.1f,
-        // RED);
 
-        
-        
-           // DrawMesh(quadMesh, backMat, transform3);
+DrawMesh(quadMesh, frontMat, frontTransform);
+DrawMesh(quadMesh, backMat, backTransform);
+DrawMesh(quadMesh, leftMat, leftTransform);
+DrawMesh(quadMesh, rightMat, rightTransform);
 
-        //       DrawCube(
-        // (Vector3){0,2,0},
-        // 1.0f,
-        // 2.0f,
-        // 0.1f,
-        // PURPLE);
-        // DrawCube(
-        //     (Vector3){0,2,0},
-        //     1.0f,
-        //     2.0f,
-        //     0.1f,
-        //     PURPLE);
-
-        //   DrawMesh(quadMesh, material, MatrixTranslate(player.position.x, player.position.y + 2.0f, player.position.z));
-        // DrawBillboardRec(
-        //     camera,
-        //     hero,
-        //     source,
-        //     player.position,
-        //     (Vector2){2.0f, 4.0f},
-        //     WHITE);
-
+        rlEnableBackfaceCulling();
         EndMode3D();
 
         GuiGroupBox(
-            (Rectangle){5, 70, 240, 170},
-            "Debug UI");
+            (Rectangle){5, 70, 350, 520},
+            "Sprite Debug");
 
-        GuiSlider(
-            (Rectangle){10, 120, 200, 20},
-            "Slow",
-            "Fast",
-            &player.speed,
-            0.01f,
-            1.0f);
+        // GuiSlider(
+        //     (Rectangle){10, 120, 200, 20},
+        //     "Slow",
+        //     "Fast",
+        //     &player.speed,
+        //     0.01f,
+        //     1.0f);
 
-        // DrawText(
-        //     TextFormat(
-        //         "Direction: %d",
-        //         direction),
-        //     10,
-        //     35,
-        //     20,
-        //   BLACK);
         DrawText(
             TextFormat("Angle Degrees: %.2f", angleDegrees),
             20,
@@ -264,3 +330,204 @@ Matrix backTransform =
 
     return 0;
 }
+
+/*
+
+    // front control
+        DrawText("Front X", 15, 100, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 100, 200, 20},
+            "",
+            "",
+            &frontOffsetX,
+            -1.0f,
+            1.0f);
+
+        DrawText("Front Y", 15, 130, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 130, 200, 20},
+            "",
+            "",
+            &frontOffsetY,
+            0.0f,
+            3.0f);
+
+        DrawText("Front Z", 15, 160, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 160, 200, 20},
+            "",
+            "",
+            &frontOffsetZ,
+            -1.0f,
+            1.0f);
+        // side controle
+
+        DrawText("Side X", 15, 210, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 210, 200, 20},
+            "",
+            "",
+            &sideOffsetX,
+            -1.0f,
+            1.0f);
+
+        DrawText("Side Y", 15, 240, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 240, 200, 20},
+            "",
+            "",
+            &sideOffsetY,
+            0.0f,
+            3.0f);
+
+        DrawText("Side Z", 15, 270, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 270, 200, 20},
+            "",
+            "",
+            &sideOffsetZ,
+            -1.0f,
+            1.0f);
+        // back controle
+
+        DrawText("Back X", 15, 320, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 320, 200, 20},
+            "",
+            "",
+            &backOffsetX,
+            -1.0f,
+            1.0f);
+
+        DrawText("Back Y", 15, 350, 10, BLACK);
+        GuiSlider(
+            (Rectangle){80, 350, 200, 20},
+            "",
+            "",
+            &backOffsetY,
+            0.0f,
+            3.0f);
+
+        DrawText("Back Z", 15, 380, 10, BLACK);
+
+        GuiSlider(
+            (Rectangle){80, 380, 200, 20},
+            "",
+            "",
+            &backOffsetZ,
+            -1.0f,
+            1.0f);
+
+        /// rotation
+
+        // front
+        GuiSlider(
+            (Rectangle){80, 180, 200, 20},
+            "",
+            "",
+            &frontRotX,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 210, 200, 20},
+            "",
+            "",
+            &frontRotY,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 240, 200, 20},
+            "",
+            "",
+            &frontRotZ,
+            -180,
+            180);
+
+        // side
+
+        GuiSlider(
+            (Rectangle){80, 300, 200, 20},
+            "",
+            "",
+            &sideRotX,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 330, 200, 20},
+            "",
+            "",
+            &sideRotY,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 360, 200, 20},
+            "",
+            "",
+            &sideRotZ,
+            -180,
+            180);
+
+        // back
+
+        GuiSlider(
+            (Rectangle){80, 420, 200, 20},
+            "",
+            "",
+            &backRotX,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 450, 200, 20},
+            "",
+            "",
+            &backRotY,
+            -180,
+            180);
+
+        GuiSlider(
+            (Rectangle){80, 480, 200, 20},
+            "",
+            "",
+            &backRotZ,
+            -180,
+            180);
+
+        // draw current value
+
+        DrawText(
+            TextFormat("F %.2f %.2f %.2f",
+                       frontOffsetX,
+                       frontOffsetY,
+                       frontOffsetZ),
+            15,
+            420,
+            10,
+            BLACK);
+
+        DrawText(
+            TextFormat("S %.2f %.2f %.2f",
+                       sideOffsetX,
+                       sideOffsetY,
+                       sideOffsetZ),
+            15,
+            435,
+            10,
+            BLACK);
+
+        DrawText(
+            TextFormat("B %.2f %.2f %.2f",
+                       backOffsetX,
+                       backOffsetY,
+                       backOffsetZ),
+            15,
+            450,
+            10,
+            BLACK);
+
+
+*/
