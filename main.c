f52d82cb96711b395d3e955741122cd3adc81da8
aezanpathan <aezanp@gmail.com>
2026-06-13 18:47:05 +0530

    fix the issue of the side sprite

--- main.c
+++ main.c
@@ -134,7 +134,7 @@ int main(void)
     // for the debug purpose
     float frontOffsetX = 0.0f;
     float frontOffsetY = 1.0f;
-    float frontOffsetZ = 0.12f;
+    float frontOffsetZ = 0.15f;
 
     float sideOffsetX = 0.0f;
     float sideOffsetY = 1.0f;
@@ -142,7 +142,7 @@ int main(void)
 
     float backOffsetX = 0.0f;
     float backOffsetY = 1.0f;
-    float backOffsetZ = -0.12f;
+    float backOffsetZ = -0.15f;
 
     // rotation
     float frontRotX = 90.0f;
@@ -186,67 +186,34 @@ int main(void)
             camera.position.x - player.position.x,
             camera.position.z - player.position.z);
 
-        // Matrix frontTransform =
-        //     MatrixTranslate(
-        //         player.position.x + frontOffsetX,
-        //         player.position.y + frontOffsetY,
-        //         player.position.z + frontOffsetZ);
-
-        // Matrix sideTransform =
-        //     MatrixTranslate(
-        //         player.position.x + sideOffsetX,
-        //         player.position.y + sideOffsetY,
-        //         player.position.z + sideOffsetZ);
-
-        // Matrix backTransform =
-        //     MatrixTranslate(
-        //         player.position.x + backOffsetX,
-        //         player.position.y + backOffsetY,
-        //         player.position.z + backOffsetZ);
-
         Matrix frontTransform =
-            MatrixMultiply(
-                MatrixRotateX(DEG2RAD * 90.0f),
-                MatrixTranslate(
-                    player.position.x,
-                    player.position.y + 1.0f,
-                    player.position.z + shellDepth));
+            MatrixTranslate(
+                player.position.x + frontOffsetX,
+                player.position.y + frontOffsetY,
+                player.position.z + frontOffsetZ);
+
+        Matrix sideTransform =
+            MatrixTranslate(
+                player.position.x + sideOffsetX,
+                player.position.y + sideOffsetY,
+                player.position.z + sideOffsetZ);
 
         Matrix backTransform =
-            MatrixMultiply(
-                MatrixRotateY(DEG2RAD * 180.0f),
-                MatrixMultiply(
-                    MatrixRotateX(DEG2RAD * 90.0f),
-                    MatrixTranslate(
-                        player.position.x,
-                        player.position.y + 1.0f,
-                        player.position.z - shellDepth)));
-
-        Matrix leftTransform =
-            MatrixMultiply(
-                MatrixRotateY(DEG2RAD * 90.0f),
-                MatrixMultiply(
-                    MatrixRotateX(DEG2RAD * 90.0f),
-                    MatrixTranslate(
-                        player.position.x - shellDepth,
-                        player.position.y + 1.0f,
-                        player.position.z)));
-
-        Matrix rightTransform =
-            MatrixMultiply(
-                MatrixRotateY(DEG2RAD * -90.0f),
-                MatrixMultiply(
-                    MatrixRotateX(DEG2RAD * 90.0f),
-                    MatrixTranslate(
-                        player.position.x + shellDepth,
-                        player.position.y + 1.0f,
-                        player.position.z)));
+            MatrixTranslate(
+                player.position.x + backOffsetX,
+                player.position.y + backOffsetY,
+                player.position.z + backOffsetZ);
         Matrix frontRotation = MatrixRotateX(DEG2RAD * 90.0f);
-        Matrix sideRotation = MatrixMultiply(MatrixRotateY(billboardYaw), MatrixRotateX(DEG2RAD * 90.0f));
+       // Matrix sideRotation = MatrixMultiply(MatrixRotateY(billboardYaw), MatrixRotateX(DEG2RAD * 90.0f));
         Matrix backRotation = MatrixRotateX(DEG2RAD * 90.0f);
 
+     Matrix sideRotation = MatrixMultiply(MatrixRotateZ(billboardYaw), MatrixRotateX(DEG2RAD * 90.0f));
+
+
+         
+
         frontTransform = MatrixMultiply(frontRotation, frontTransform);
-      //  sideTransform = MatrixMultiply(sideRotation, sideTransform);
+        sideTransform = MatrixMultiply(sideRotation, sideTransform);
         backTransform = MatrixMultiply(backRotation, backTransform);
 
         // Draw
@@ -285,20 +252,15 @@ int main(void)
 
         rlDisableBackfaceCulling();
 
-        // //  if (angleDegrees < 120.0f)
-        // DrawMesh(quadMesh, frontMat, frontTransform);
+        if (angleDegrees < 120.0f)
+        DrawMesh(quadMesh, frontMat, frontTransform);
 
-        // //   if (angleDegrees > 60.0f && angleDegrees < 300.0f)
-        // DrawMesh(quadMesh, sideMat, sideTransform);
+         if (angleDegrees > 60.0f && angleDegrees < 300.0f)
+        DrawMesh(quadMesh, sideMat, sideTransform);
 
-        // //  if (angleDegrees > 180.0f)
-        // DrawMesh(quadMesh, backMat, backTransform);
+        if (angleDegrees > 180.0f)
+        DrawMesh(quadMesh, backMat, backTransform);
 
-
-DrawMesh(quadMesh, frontMat, frontTransform);
-DrawMesh(quadMesh, backMat, backTransform);
-DrawMesh(quadMesh, leftMat, leftTransform);
-DrawMesh(quadMesh, rightMat, rightTransform);
 
         rlEnableBackfaceCulling();
         EndMode3D();
@@ -330,204 +292,3 @@ DrawMesh(quadMesh, rightMat, rightTransform);
 
     return 0;
 }
-
-/*
-
-    // front control
-        DrawText("Front X", 15, 100, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 100, 200, 20},
-            "",
-            "",
-            &frontOffsetX,
-            -1.0f,
-            1.0f);
-
-        DrawText("Front Y", 15, 130, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 130, 200, 20},
-            "",
-            "",
-            &frontOffsetY,
-            0.0f,
-            3.0f);
-
-        DrawText("Front Z", 15, 160, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 160, 200, 20},
-            "",
-            "",
-            &frontOffsetZ,
-            -1.0f,
-            1.0f);
-        // side controle
-
-        DrawText("Side X", 15, 210, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 210, 200, 20},
-            "",
-            "",
-            &sideOffsetX,
-            -1.0f,
-            1.0f);
-
-        DrawText("Side Y", 15, 240, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 240, 200, 20},
-            "",
-            "",
-            &sideOffsetY,
-            0.0f,
-            3.0f);
-
-        DrawText("Side Z", 15, 270, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 270, 200, 20},
-            "",
-            "",
-            &sideOffsetZ,
-            -1.0f,
-            1.0f);
-        // back controle
-
-        DrawText("Back X", 15, 320, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 320, 200, 20},
-            "",
-            "",
-            &backOffsetX,
-            -1.0f,
-            1.0f);
-
-        DrawText("Back Y", 15, 350, 10, BLACK);
-        GuiSlider(
-            (Rectangle){80, 350, 200, 20},
-            "",
-            "",
-            &backOffsetY,
-            0.0f,
-            3.0f);
-
-        DrawText("Back Z", 15, 380, 10, BLACK);
-
-        GuiSlider(
-            (Rectangle){80, 380, 200, 20},
-            "",
-            "",
-            &backOffsetZ,
-            -1.0f,
-            1.0f);
-
-        /// rotation
-
-        // front
-        GuiSlider(
-            (Rectangle){80, 180, 200, 20},
-            "",
-            "",
-            &frontRotX,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 210, 200, 20},
-            "",
-            "",
-            &frontRotY,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 240, 200, 20},
-            "",
-            "",
-            &frontRotZ,
-            -180,
-            180);
-
-        // side
-
-        GuiSlider(
-            (Rectangle){80, 300, 200, 20},
-            "",
-            "",
-            &sideRotX,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 330, 200, 20},
-            "",
-            "",
-            &sideRotY,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 360, 200, 20},
-            "",
-            "",
-            &sideRotZ,
-            -180,
-            180);
-
-        // back
-
-        GuiSlider(
-            (Rectangle){80, 420, 200, 20},
-            "",
-            "",
-            &backRotX,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 450, 200, 20},
-            "",
-            "",
-            &backRotY,
-            -180,
-            180);
-
-        GuiSlider(
-            (Rectangle){80, 480, 200, 20},
-            "",
-            "",
-            &backRotZ,
-            -180,
-            180);
-
-        // draw current value
-
-        DrawText(
-            TextFormat("F %.2f %.2f %.2f",
-                       frontOffsetX,
-                       frontOffsetY,
-                       frontOffsetZ),
-            15,
-            420,
-            10,
-            BLACK);
-
-        DrawText(
-            TextFormat("S %.2f %.2f %.2f",
-                       sideOffsetX,
-                       sideOffsetY,
-                       sideOffsetZ),
-            15,
-            435,
-            10,
-            BLACK);
-
-        DrawText(
-            TextFormat("B %.2f %.2f %.2f",
-                       backOffsetX,
-                       backOffsetY,
-                       backOffsetZ),
-            15,
-            450,
-            10,
-            BLACK);
-
-
-*/
